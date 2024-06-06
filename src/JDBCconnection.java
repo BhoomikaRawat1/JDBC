2fc3e7fe35dc1cf09113476053e1696ec5e29e34
BhoomikaRawat1 <br2227469@gmail.com>
2024-06-06 14:25:37 +0530

    first commit

--- src/JDBCconnection.java
+++ src/JDBCconnection.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class JDBCconnection {
-}
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JDBCconnection {
+        //private static String username;
+
+        public static void main(String[] args) {
+            // Database URl
+            String url="jdbc:mysql://localhost:3306/students";
+            // Database credentials
+            String user ="root";
+            String password="2005";
+
+            try(Connection connection = DriverManager.getConnection(url,user,password)){
+
+                System.out.println("connected to the mysql is successfully");
+                System.out.println(connection);
+            }
+            catch(SQLException e){
+                System.err.println("Connection failed:" + e.getMessage());
+            }
+        }
+    }
